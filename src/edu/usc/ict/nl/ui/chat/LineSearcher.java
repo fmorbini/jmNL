b05628bdbc70404320162aae82c2f5f89f56f109
fabrizio morbini <fmorbini@gmail.com>
2015-10-23 14:30:58 -0700

    added search function for nlg tester.

---
+++ src/edu/usc/ict/nl/ui/chat/LineSearcher.java
@@ -0,0 +1,107 @@
+package edu.usc.ict.nl.ui.chat;
+
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field.Store;
+import org.apache.lucene.document.IntField;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
+
+import edu.usc.ict.nl.nlg.SpeechActWithProperties;
+import edu.usc.ict.nl.nlu.wikidata.dumps.LuceneQueryConstants;
+import edu.usc.ict.nl.util.StringUtils;
+
+public class LineSearcher {
+	
+	private Class analyzerClass=null;
+	protected IndexSearcher searcher;
+	private Directory index=null;
+	protected QueryParser queryParser=null;
+	private Map<Integer,SpeechActWithProperties> lines;
+
+	private static final String ID="sa";
+	private static final String ROW="row";
+	private static final String TEXT="text";
+	
+	public LineSearcher(Map<String, List<SpeechActWithProperties>> linesRaw) throws Exception {
+		analyzerClass=Class.forName("org.apache.lucene.analysis.standard.StandardAnalyzer");
+		rebuildIndex(linesRaw);
+		initSearch();
+	}
+
+	private void initSearch() throws Exception {
+		searcher = new IndexSearcher(DirectoryReader.open(index));
+		Constructor constructor = analyzerClass.getConstructor();
+		queryParser = new QueryParser(TEXT, (Analyzer) constructor.newInstance());
+	}
+
+	public void rebuildIndex(Map<String, List<SpeechActWithProperties>> linesRaw) throws Exception {
+		Constructor constructor = analyzerClass.getConstructor();
+		IndexWriterConfig config = new IndexWriterConfig((Analyzer) constructor.newInstance());
+		if (index==null) index=new RAMDirectory();
+		IndexWriter writer = new IndexWriter(index, config);
+		writer.deleteAll();
+		writer.commit();
+
+		if (linesRaw!=null) {
+			for(List<SpeechActWithProperties> ls:linesRaw.values()) {
+				if (ls!=null) {
+					for(SpeechActWithProperties l:ls) {
+						if (lines==null) lines=new HashMap<>();
+						lines.put(l.getRow(), l);
+						Document doc=createDoc(l);
+						writer.addDocument(doc);
+					}
+				}
+			}
+		}
+		writer.commit();
+		writer.close();
+	}
+	
+
+	protected Document createDoc(SpeechActWithProperties l) {
+		Document doc = new Document();
+		doc.add(new IntField(ROW, l.getRow(), Store.YES));
+		doc.add(new StringField(ID, l.getSA().toLowerCase(), Store.NO));
+		doc.add(new TextField(TEXT, LuceneQueryConstants.START+" "+l.getText().toLowerCase()+" "+LuceneQueryConstants.END, Store.NO));
+		return doc;
+	}
+
+	public List<SpeechActWithProperties> find(String query,int n) throws Exception {
+		query=StringUtils.removeLeadingAndTrailingSpaces(query);
+		if (query.startsWith("^")) query=LuceneQueryConstants.START+" "+query.substring(1);
+		if (query.endsWith("$")) query=query.substring(0, query.length()-1)+" "+LuceneQueryConstants.END;
+		Query q = queryParser.parse(query);
+		//System.out.println("query: "+q.getClass()+" "+q);
+		TopDocs result = searcher.search(q,n);
+		ScoreDoc[] hits = result.scoreDocs;
+		List<SpeechActWithProperties> ret=null;
+		for (int i = 0; i < hits.length; i++) {
+			Document doc = searcher.getIndexReader().document(hits[i].doc);
+			Integer row=Integer.parseInt(doc.get(ROW));
+			SpeechActWithProperties s=lines.get(row);
+			if (s!=null) {
+				if (ret==null) ret=new ArrayList<SpeechActWithProperties>();
+				ret.add(s);
+			}
+		}
+		return ret;
+	}
+}
--- src/edu/usc/ict/nl/ui/chat/LineTester.java
+++ src/edu/usc/ict/nl/ui/chat/LineTester.java
@@ -2,6 +2,7 @@ package edu.usc.ict.nl.ui.chat;
 
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -21,10 +22,14 @@ import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
+import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 import javax.swing.SwingConstants;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import javax.swing.text.Document;
 
 import edu.usc.ict.nl.bus.NLBusInterface;
 import edu.usc.ict.nl.bus.events.DMSpeakEvent;
@@ -32,18 +37,20 @@ import edu.usc.ict.nl.bus.events.NLGEvent;
 import edu.usc.ict.nl.bus.modules.NLG;
 import edu.usc.ict.nl.bus.modules.NLGInterface;
 import edu.usc.ict.nl.nlg.SpeechActWithProperties;
+import edu.usc.ict.nl.util.StringUtils;
 
-public class LineTester extends JPanel implements ListSelectionListener {
+public class LineTester extends JPanel implements ListSelectionListener, ActionListener, DocumentListener {
 	private JList list;
 	private DefaultListModel listModel;
 	private JLabel extraInfoLabel=null;
 
 	private Long sessionID=null;
 	private NLBusInterface bus=null;
+	private LineSearcher search;
+	private List<SpeechActWithProperties> lines=new ArrayList<SpeechActWithProperties>();
 
 	public LineTester(final NLBusInterface nlModule, final Long sid, final Map<String, List<SpeechActWithProperties>> linesRaw) throws Exception {
 		super(new BorderLayout());
-		List<SpeechActWithProperties> lines=new ArrayList<SpeechActWithProperties>();
 		if (linesRaw!=null) {
 			for(List<SpeechActWithProperties> ls:linesRaw.values()) {
 				lines.addAll(ls);
@@ -58,14 +65,17 @@ public class LineTester extends JPanel implements ListSelectionListener {
 			}
 		});
 		
+		search=new LineSearcher(linesRaw);
 		this.bus=nlModule;
 		this.sessionID=sid;
 
+		
+		JTextField searchBox = new JTextField();
+		searchBox.addActionListener(this);
+		searchBox.getDocument().addDocumentListener(this);
+
 		listModel = new DefaultListModel();
-		for(SpeechActWithProperties l:lines) {
-			//ev.setSessionID(sessionID);
-			listModel.addElement(l);
-		}
+		addLines();
 
 		//Create the list and put it in a scroll pane.
 		list = new JList(listModel);
@@ -111,9 +121,15 @@ public class LineTester extends JPanel implements ListSelectionListener {
 		buttonPane.add(Box.createHorizontalStrut(5));
 		buttonPane.add(extraInfoLabel);
 		buttonPane.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
-
+		add(searchBox,BorderLayout.NORTH);
 		add(listScrollPane, BorderLayout.CENTER);
 		add(buttonPane, BorderLayout.PAGE_END);
+	}
+
+	private void addLines() {
+		for(SpeechActWithProperties l:lines) {
+			listModel.addElement(l);
+		}
 	}
 
 	private void updateSelectedIndex(int index) {
@@ -128,9 +144,12 @@ public class LineTester extends JPanel implements ListSelectionListener {
 	//This method is required by ListSelectionListener.
 	public void valueChanged(ListSelectionEvent e) {
 		if (e.getValueIsAdjusting() == false && extraInfoLabel!=null) {
-			SpeechActWithProperties ev = (SpeechActWithProperties)listModel.getElementAt(list.getSelectedIndex());
-			if (ev!=null) {
-				extraInfoLabel.setText(ev.toString(true));
+			int p = list.getSelectedIndex();
+			if (p>=0) {
+				SpeechActWithProperties ev = (SpeechActWithProperties)listModel.getElementAt(p);
+				if (ev!=null) {
+					extraInfoLabel.setText(ev.toString(true));
+				}
 			}
 		}
 	}
@@ -162,4 +181,52 @@ public class LineTester extends JPanel implements ListSelectionListener {
 			}
 		});
 	}
+
+	public List<SpeechActWithProperties> findAndUpdateList(Document searchBox) {
+		try {
+			String query=searchBox.getText(0, searchBox.getLength());
+			if (StringUtils.isEmptyString(query)) {
+				listModel.clear();
+				addLines();
+			} else {
+				List<SpeechActWithProperties> result = search.find(query,Integer.MAX_VALUE);
+				listModel.clear();
+				if (result!=null) {
+					for(SpeechActWithProperties c:result) {
+						listModel.addElement(c);
+					}
+				}
+				return result;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	@Override
+	public void insertUpdate(DocumentEvent e) {
+		Document doc = e.getDocument();
+		findAndUpdateList(doc);
+	}
+
+	@Override
+	public void removeUpdate(DocumentEvent e) {
+		Document doc = e.getDocument();
+		findAndUpdateList(doc);
+	}
+
+	@Override
+	public void changedUpdate(DocumentEvent e) {
+		Document doc = e.getDocument();
+		findAndUpdateList(doc);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
 }
